dc08025bdfdab7247ab9d0c1b9c786285fe578e0
Mahmoud Shata <83148301+MahmoudShata23@users.noreply.github.com>
2022-03-20 15:15:13 +0200

    adddddd

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -25,26 +25,26 @@ class ProductController extends Controller
     public function StoreProduct(Request $request)
     {
         $validator = $request->validate([
-            'brand'=>'required',
-            'category_id'=>'required',
-            'sub_category_id'=>'required',
-            'name'=>'required|max:191',
-            'description'=>'required|max:191',
-            'product_qty'=>'required|max:20',
-            'tags'=>'required|max:20',
-            'product_size'=>'required|max:191',
-            'color'=>'required|max:191',
-            'selling_price'=>'required|max:20',
-            'discount_price'=>'required|max:20',
-            'short_desc'=>'required|max:191',
-            'status'=>'required',
-            'avgRate'=>'required',
-            'image'=>'required|max:2048|image|mimes:png,jpg,jpeg',
+            'brand' => 'required',
+            'category_id' => 'required',
+            'sub_category_id' => 'required',
+            'name' => 'required|max:191',
+            'description' => 'required|max:191',
+            'product_qty' => 'required|max:20',
+            'tags' => 'required|max:20',
+            'product_size' => 'required|max:191',
+            'color' => 'required|max:191',
+            'selling_price' => 'required|max:20',
+            'discount_price' => 'required|max:20',
+            'short_desc' => 'required|max:191',
+            'status' => 'required',
+            'avgRate' => 'required',
+            'image' => 'required|max:2048|image|mimes:png,jpg,jpeg',
 
         ]);
-        if($image = $request->image){
+        if ($image = $request->image) {
             $destinationPath = 'uploads/product';
-            $newImage= time() . "." . $image->getClientOriginalExtension();
+            $newImage = time() . "." . $image->getClientOriginalExtension();
             $image->move($destinationPath, $newImage);
             $validator['image'] = $newImage;
         }
@@ -59,27 +59,25 @@ class ProductController extends Controller
                 'status' => 422,
                 'errors' => $validator->messages()
             ]);
-
-    }
-
+        }
     }
     public function MultiImageUpdate(Request $request)
     {
-
     }
     public function ShowProduct($id)
     {
         $product = Product::find($id);
 
-        if($product){
+        if ($product) {
             return response()->json([
-                'status'=>200,
-                '$product'=>$product ]);
-        }
-        else {
+                'status' => 200,
+                '$product' => $product
+            ]);
+        } else {
             return response()->json([
-                'status'=>404,
-                'message'=>'No product id found' ]);
+                'status' => 404,
+                'message' => 'No product id found'
+            ]);
         }
     }
     public function EditProduct($id)
@@ -87,66 +85,67 @@ class ProductController extends Controller
 
         $product = Product::find($id);
 
-        if($product){
+        if ($product) {
             return response()->json([
-                'status'=>200,
-                '$product'=>$product ]);
-        }
-        else {
+                'status' => 200,
+                '$product' => $product
+            ]);
+        } else {
             return response()->json([
-                'status'=>404,
-                'message'=>'No product id found' ]);
+                'status' => 404,
+                'message' => 'No product id found'
+            ]);
         }
-
     }
     public function UpdateProduct(Request $request, $id)
     {
         $validator = $request->validate([
-            'brand'=>'required',
-            'category_id'=>'required',
-            'sub_category_id'=>'required',
-            'name'=>'required|max:191',
-            'description'=>'required|max:191',
-            'product_qty'=>'required|max:20',
-            'tags'=>'required|max:20',
-            'product_size'=>'required|max:191',
-            'color'=>'required|max:191',
-            'selling_price'=>'required|max:20',
-            'discount_price'=>'required|max:20',
-            'short_desc'=>'required|max:191',
-            'status'=>'required',
-            'avgRate'=>'required',
-            'image'=>'required|max:2048|image|mimes:png,jpg,jpeg',
+            'brand' => 'required',
+            'category_id' => 'required',
+            'sub_category_id' => 'required',
+            'name' => 'required|max:191',
+            'description' => 'required|max:191',
+            'product_qty' => 'required|max:20',
+            'tags' => 'required|max:20',
+            'product_size' => 'required|max:191',
+            'color' => 'required|max:191',
+            'selling_price' => 'required|max:20',
+            'discount_price' => 'required|max:20',
+            'short_desc' => 'required|max:191',
+            'status' => 'required',
+            'avgRate' => 'required',
+            'image' => 'required',
 
         ]);
-        $op = Product::where('id',$request->id)->update($validator);
-        if ($op){
+        $op = Product::where('id', $request->id)->update($validator);
+        if ($op) {
             return response()->json([
                 'status' => 200,
                 'message' => 'Product updated succesfully',
             ]);
-        }else{
+        } else {
             return response()->json([
-                'status'=>422,
-                'errors'=> $validator->messages()
+                'status' => 422,
+                'errors' => $validator->messages()
             ]);
         }
-
     }
     public function DeleteProduct($id)
     {
         $product = Product::find($id);
 
-        if($product){
+        if ($product) {
             $product->delete();
             //unlink($product->image);
             return response()->json([
-                'status'=>200,
-                'message'=>'$product deleted succesfully']);
-        } else{
+                'status' => 200,
+                'message' => '$product deleted succesfully'
+            ]);
+        } else {
             return response()->json([
-                'status'=>404,
-                'message'=>'$product ID not found']);
+                'status' => 404,
+                'message' => '$product ID not found'
+            ]);
         }
     }
     // product Stock
@@ -164,7 +163,6 @@ class ProductController extends Controller
                 'status' => 422,
                 'errors' => 'not product update found'
             ]);
-
         }
     }
 }
