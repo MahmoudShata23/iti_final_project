b8d1fdd5d267faf317201426845b1d020ea16cfa
Nada <nadaua2@gmail.com>
2022-04-04 15:47:47 +0200

    update

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\OrderItemResource;
 use  App\Models\Order;
 use  App\Models\OrderItem;
 use  App\Models\Product;
